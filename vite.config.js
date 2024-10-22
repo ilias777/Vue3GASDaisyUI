78d4017920f482ea76986845060f5d0bd747ea73
Ilias <dougias.ilias@gmail.com>
2024-10-22 08:12:02 +0200

    feat(plugin): Use singlefile plugin

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,14 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue(), viteSingleFile()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 })
